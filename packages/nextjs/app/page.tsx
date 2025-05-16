6a61d01d570682b6e7c1ab9d2995ad7ec4823302
Andrea Albiac <andrealbiac@gmail.com>
2025-05-16 16:40:56 +0200

    Resetted to clien'ts site design

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,70 +1,56 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
 import { NextPage } from "next";
-import bgrpcLogo from "~~/public/bgrpc.png";
 
 const Home: NextPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
     <div className="container mx-auto">
       {/* Header with fixed logo */}
       <header className="container mx-auto pb-24 md:pb-32 lg:pb-36 border-l border-r border-black">
-        <div className="fixed container z-10 mix-blend-difference p-6 lg:p-8">
-          <Image className="w-40 md:w-auto invert" src={bgrpcLogo} alt="logo" width={260} height={78} />
+        <div className="fixed container z-10 p-6 lg:p-8">
+          <Image className="w-40" src="rpc-logo.svg" alt="logo" width={260} height={78} />
         </div>
       </header>
-
       {/* First row */}
       <div className="flex flex-col lg:flex-row lg:border-x-[1px] lg:border-y-[1px] border-black">
-        {/* Satellite section - now on the left */}
-        <section className="bg-[#20F658] p-6 w-full flex justify-center border-x-[1px] border-y-[1px] border-black lg:border-none lg:w-[55vw]">
-          <Image src="/satellite-10fps.gif" alt="satellite" className="object-contain" width={436} height={535} />
-        </section>
-
-        {/* Instructions section - now on the right */}
-        <section className="bg-[#20F658] p-6 lg:p-10 w-full lg:w-[45vw] border-x-[1px] border-y-[1px] border-black lg:border-none overflow-auto flex items-center justify-center">
+        {/* Introduction section */}
+        <section className="bg-[#df57c4] p-6 lg:p-10 w-full lg:w-[45vw] border-x-[1px] border-y-[1px] border-black lg:border-none overflow-auto">
           <div className="flex flex-col">
-            <div className="mb-6 flex justify-center">
-              <Image src="crosses-1.svg" alt="crosses" className="w-[200px] lg:w-[400px]" width={306} height={50} />
-            </div>
-            <p className="mt-0">Power your decentralized apps with:</p>
+            <p className="mt-0">Run an Ethereum node with a single command:</p>
+            <p className="mt-0">Mac/linux</p>
             <div className="bg-black p-2 lg:p-4 text-white text-sm overflow-auto">
-              <p className="m-2">https://mainnet.rpc.buidlguidl.com</p>
+              <p className="m-2">/bin/bash -c &quot;$(curl -fsSL https://bgclient.io)&quot;</p>
             </div>
-            <div className="mt-6 flex justify-center">
-              <Image src="crosses-2.svg" alt="crosses" className="w-[200px] lg:w-[400px]" width={306} height={50} />
+            <p> or run the client from the repo:</p>
+            <div className="bg-black p-2 lg:p-4 text-white text-sm overflow-auto">
+              <p className="m-2 whitespace-nowrap">git clone https://github.com/BuidlGuidl/buidlguidl-client.git</p>
+              <p className="m-2">cd buidlguidl-client</p>
+              <p className="m-2">yarn install</p>
+              <p className="m-2">node index.js</p>
             </div>
           </div>
         </section>
-      </div>
 
-      {/* Modal */}
-      {isModalOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-85 z-50 flex items-center justify-center p-4"
-          onClick={() => setIsModalOpen(false)}
-        >
-          <div className="relative max-w-[90vw] max-h-[90vh]">
+        {/* Second row for mobile - flex row to make sections share the row */}
+        <div className="flex flex-row flex-1">
+          {/* Screenshot section */}
+          <section className="bg-[#DDDDDD] w-7/12 lg:flex-1 p-6 flex justify-center border-x-[1px] border-b-[1px] border-black lg:border-b-0">
             <Image
-              src="/screenshot-3-modal.png"
+              src="/screenshot-3.png"
               alt="screenshot"
-              className="object-contain"
-              width={2030}
-              height={1327}
-              onClick={e => e.stopPropagation()}
+              className="object-contain cursor-pointer"
+              width={972}
+              height={875}
             />
-            <button
-              className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full w-8 h-8 flex items-center justify-center"
-              onClick={() => setIsModalOpen(false)}
-            >
-              ✕
-            </button>
-          </div>
+          </section>
+
+          {/* Satellite section */}
+          <section className="bg-[#87A0F9] p-6 w-5/12 lg:flex-1 flex justify-center border-r-[1px] border-b-[1px] border-black lg:border-r-0 lg:border-l-0 lg:border-b-0">
+            <Image src="/satellite-10fps.gif" alt="satellite" className="object-contain" width={436} height={535} />
+          </section>
         </div>
-      )}
+      </div>
     </div>
   );
 };
