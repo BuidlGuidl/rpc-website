280ca979d311af4ff111cf29befa24d3ce953597
Andrea Albiac <andrealbiac@gmail.com>
2025-05-16 17:07:28 +0200

    Updated sections content and style

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -12,42 +12,35 @@ const Home: NextPage = () => {
           <Image className="w-40" src="rpc-logo.svg" alt="logo" width={260} height={78} />
         </div>
       </header>
+
       {/* First row */}
       <div className="flex flex-col lg:flex-row lg:border-x-[1px] lg:border-y-[1px] border-black">
         {/* Introduction section */}
-        <section className="bg-[#df57c4] p-6 lg:p-10 w-full lg:w-[45vw] border-x-[1px] border-y-[1px] border-black lg:border-none overflow-auto">
+        <section className="bg-[#DDDDDD] p-6 lg:p-10 w-full lg:w-[60vw] border-x-[1px] border-y-[1px] border-black lg:border-none overflow-auto">
           <div className="flex flex-col">
-            <p className="mt-0">Run an Ethereum node with a single command:</p>
-            <p className="mt-0">Mac/linux</p>
+            <p className="mt-0">
+              A one line command to deploy and monitor an Ethereum Node, funded and maintained by BuidlGuidl members.
+            </p>
+            <p className="mt-0">Power your decentralized apps with:</p>
             <div className="bg-black p-2 lg:p-4 text-white text-sm overflow-auto">
-              <p className="m-2">/bin/bash -c &quot;$(curl -fsSL https://bgclient.io)&quot;</p>
+              <p className="m-2">https://mainnet.rpc.buidlguidl.com</p>
             </div>
-            <p> or run the client from the repo:</p>
-            <div className="bg-black p-2 lg:p-4 text-white text-sm overflow-auto">
-              <p className="m-2 whitespace-nowrap">git clone https://github.com/BuidlGuidl/buidlguidl-client.git</p>
-              <p className="m-2">cd buidlguidl-client</p>
-              <p className="m-2">yarn install</p>
-              <p className="m-2">node index.js</p>
-            </div>
+            <p> Powered by worldwide BG Clients.</p>
           </div>
         </section>
 
         {/* Second row for mobile - flex row to make sections share the row */}
-        <div className="flex flex-row flex-1">
-          {/* Screenshot section */}
-          <section className="bg-[#DDDDDD] w-7/12 lg:flex-1 p-6 flex justify-center border-x-[1px] border-b-[1px] border-black lg:border-b-0">
-            <Image
-              src="/screenshot-3.png"
-              alt="screenshot"
-              className="object-contain cursor-pointer"
-              width={972}
-              height={875}
-            />
+        <div className="flex flex-col flex-1">
+          {/* Satellite section */}
+          <section className="bg-[#20F658] p-6 flex justify-center border-r-[1px] border-l-[1px] border-b-[1px] border-black lg:border-r-0">
+            <Image src="/satellite-10fps.gif" alt="satellite" className="object-contain" width={436} height={535} />
           </section>
 
-          {/* Satellite section */}
-          <section className="bg-[#87A0F9] p-6 w-5/12 lg:flex-1 flex justify-center border-r-[1px] border-b-[1px] border-black lg:border-r-0 lg:border-l-0 lg:border-b-0">
-            <Image src="/satellite-10fps.gif" alt="satellite" className="object-contain" width={436} height={535} />
+          {/* Button section */}
+          <section className="bg-[#DDDDDD] flex justify-center border-x-[1px] border-b-[1px] border-black lg:border-b-0 lg:border-r-0">
+            <button className="bg-white border h-16 w-full flex items-center justify-center hover:bg-gray-100 transition-colors">
+              <p>Visit Client site</p>
+            </button>
           </section>
         </div>
       </div>
