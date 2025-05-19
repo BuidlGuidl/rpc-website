3bead5cea56faa60d0081dc3ece6875612b42164
Andrea Albiac <andrealbiac@gmail.com>
2025-05-19 15:10:22 +0200

    Removed dark mode

--- packages/nextjs/hooks/scaffold-eth/useNetworkColor.ts
+++ packages/nextjs/hooks/scaffold-eth/useNetworkColor.ts
@@ -1,23 +1,17 @@
-import { useTargetNetwork } from "./useTargetNetwork";
-import { useTheme } from "next-themes";
 import { useSelectedNetwork } from "~~/hooks/scaffold-eth";
 import { AllowedChainIds, ChainWithAttributes } from "~~/utils/scaffold-eth";
 
-export const DEFAULT_NETWORK_COLOR: [string, string] = ["#666666", "#bbbbbb"];
+export const DEFAULT_NETWORK_COLOR = "#666666";
 
-export function getNetworkColor(network: ChainWithAttributes, isDarkMode: boolean) {
+export function getNetworkColor(network: ChainWithAttributes) {
   const colorConfig = network.color ?? DEFAULT_NETWORK_COLOR;
-  return Array.isArray(colorConfig) ? (isDarkMode ? colorConfig[1] : colorConfig[0]) : colorConfig;
+  return Array.isArray(colorConfig) ? colorConfig[0] : colorConfig;
 }
 
 /**
  * Gets the color of the target network
  */
 export const useNetworkColor = (chainId?: AllowedChainIds) => {
-  const { resolvedTheme } = useTheme();
-
   const chain = useSelectedNetwork(chainId);
-  const isDarkMode = resolvedTheme === "dark";
-
-  return getNetworkColor(chain, isDarkMode);
+  return getNetworkColor(chain);
 };
